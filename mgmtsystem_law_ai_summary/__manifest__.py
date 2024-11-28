dd6d7615c697d842a4ffeedcebe868394c96426d
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-11-28 13:50:00 +0100

    Update __manifest__.py

--- mgmtsystem_law_ai_summary/__manifest__.py
+++ mgmtsystem_law_ai_summary/__manifest__.py
@@ -22,7 +22,7 @@
 # https://www.odoo.com/documentation/14.0/reference/module.html
 #
 {
-    'name': '',
+    'name': 'Management Law AI Summary',
     'version': '1.0',
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -33,7 +33,7 @@
     """,
     #'sequence': 1,
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/odoo-',
+    'website': 'https://vertel.se/apps/odoo-management-system',
     'images': ['static/description/banner.png'], # 560x280
     'license': 'AGPL-3',
     'depends': ["mgmtsystem_law"],
