05351ba202f198a0d1d132bb793a1d0725466048
marcuszxc <marcuszxc10@gamil.com>
2025-01-29 13:23:42 +0000

    Ported from 16 to 18

---
+++ mgmtsystem_nonconformity_qr/__manifest__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Management: Nonconformity QR',
+    'version': '0.1',
+    'summary': ' Management System Nonconformity QR.',
+    'category': 'Management',
+    'description': """
+        Management System Nonconformity QR
+    """,
+    # 'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-management-system/mgmtsystem_nonconformity_qr',
+    # 'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-management-system',
+    'depends': ['mgmtsystem', 'mgmtsystem_nonconformity'],
+    'data': [
+        'views/nonconformity_qr_view.xml',
+        'views/templates.xml',
+        'security/ir.model.access.csv'
+    ],
+    'application': False,
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
+
+
+
+
