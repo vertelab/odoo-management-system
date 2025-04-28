7ff735838bb154efd9dc200bc3919db033c3f8c5
vertelbot <vertelbot@vertel.se>
2025-04-28 06:20:21 +0000

    odoobranchpfile odoo-management-system from 18.0. Do not p-file sync

--- mgmtsystem_law_ai_chat/models/ai.py
+++ mgmtsystem_law_ai_chat/models/ai.py
@@ -28,17 +28,6 @@ class AIAgent(models.Model):
         ondelete={'mgmt-law-chat': 'cascade'}
     )
 
-    def agent_extra_context(self, quest, record=None):
-        res = super().agent_extra_context(quest=quest, record=record)
-        if self.ai_type == "mgmt-law-chat":
-            document_law_id = self.env['document.law'].search([('ai_quest_id', '=', quest.id)], limit=1)
-            if document_law_id:
-                res['Management Law Title'] = document_law_id.rss_titel
-                res['Management Law Content'] = document_law_id.rss_html
-        return res
-
-
-
 
 class AIQuest(models.Model):
     _inherit = "ai.quest"
--- mgmtsystem_law_ai_chat/models/document_law.py
+++ mgmtsystem_law_ai_chat/models/document_law.py
@@ -11,50 +11,5 @@ class DocumentLaw(models.Model):
 
     ai_quest_id = fields.Many2one(comodel_name='ai.quest',string="",help="")
 
-    @api.onchange("stage")
-    def _onchange_stage_id(self):
-        if not self.stage not in ['draft', 'cancel', 'done']:
-            if self.ai_quest_id:
-                self.ai_quest_id.status = 'active'
-                self.ai_quest_id.channel_id.write({'active': True,})
-        else:
-            if self.ai_quest_id.channel_id:
-                self.ai_quest_id.status = 'done'
-                self.ai_quest_id.channel_id.write({'active': False})
-
-
-    @api.onchange("rss_titel")
-    def _onchange_name(self):
-        if self.ai_quest_id:
-            self.ai_quest_id.write({'name': f"{self.rss_titel[:20]}"})
-            if self.ai_quest_id.channel_id:
-                self.ai_quest_id.channel_id.write({'name': f"{self.rss_titel[:20]}"})
-
-
-            
-    def write(self, vals):
-        result = super(DocumentLaw, self).write(vals)
-        for ticket in self:
-            if not self.stage not in ['draft', 'cancel', 'done']:
-                if not ticket.ai_quest_id:
-                    ticket.ai_quest_id = self.env['ai.quest'].create({
-                        'name': f"{self.rss_titel[:20]}",
-                        'sub_description': f"{self.rss_titel}",
-                        'ai_type': 'mgmt-law-chat',
-                        'init_type': 'channel',
-                        'status': 'active',
-                        'ai_agent_ids': [(
-                            0, 0, {'ai_agent_id': self.env.ref('mgmtsystem_law_ai_chat.ai_agent_mgmt_law_chat').id}
-                        )],
-                        'code': """result = quest.build(session=session,message=message_body).invoke(message_invoke)""",
-                        'description': 'Answer my questions {message}',
-                    })
-
-                    ticket.ai_quest_id.channel_id = self.env['mail.channel'].create({
-                        'name': f"{self.rss_titel[:20]}",
-                        'ai_quest_id': ticket.ai_quest_id.id,
-                        'description': f"{self.rss_titel}",
-                    }).id
-        return result
 
 
