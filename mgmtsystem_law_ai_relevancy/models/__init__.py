a3ea776790cfeb3e0c68cf297f5a6857b4aa6b00
vertelbot <vertelbot@vertel.se>
2025-05-06 10:00:40 +0000

    odoobranchpfile odoo-management-system from 18.0. Do not p-file sync T/2313

--- mgmtsystem_law_ai_relevancy/models/__init__.py
+++ mgmtsystem_law_ai_relevancy/models/__init__.py
@@ -1,2 +1,3 @@
 from . import ai_agent
 from . import ai_quest
+from . import document_law
---
+++ mgmtsystem_law_ai_relevancy/models/document_law.p.py
@@ -0,0 +1,22 @@
+import logging
+
+from odoo.exceptions import ValidationError
+from odoo import models, fields, api, _
+
+_logger = logging.getLogger(__name__)
+
+class DocumentLaw(models.Model):
+    _inherit = 'document.law'
+
+    law_summary = fields.Html()
+
+    def ai_relevancy(self):
+        quest_ids = self.env["ai.quest"].search([('ai_type', '=', 'law_relevancy'),('status','=','active')])
+        for quest in quest_ids:
+            records = self.search([('stage', '=', 'draft'),('company_id', '=', quest.company_id.id)],limit=1)
+            quest.run(records=records)
+
+        
+
+
+
