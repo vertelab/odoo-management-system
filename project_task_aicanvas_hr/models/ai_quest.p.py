2ebe0a59d06a3051656a716798d247f7890550bb
vertelbot <vertelbot@vertel.se>
2025-09-25 13:52:24 +0000

    odoobranchpfile odoo-management-system from 18.0. Do not p-file sync

--- project_task_aicanvas_hr/models/ai_quest.p.py
+++ project_task_aicanvas_hr/models/ai_quest.p.py
@@ -17,17 +17,46 @@ class AIQuest(models.Model):
         message_content = last_ai_message.content
         return self.json2dict(message_content)
 
-    def _action_create_project_task_ai_canvas_idea(self, record, ideas: dict):
-        ideas = ideas.get('ideas', {})
+    def _action_create_project_task_ai_canvas_idea(self, record, canvas: dict):
+        canvas = canvas.get('canvas', {})
+
         if not record.project_id:
             raise UserError(_(f"Kindly select a Project on the department - {record.name} before you continue..."))
-        if not ideas:
+        if not canvas:
             return
-        for idea in ideas:
-            self.env['project.task'].create({
-                'name': idea.get('idea'),
-                'needs': idea.get('needs'),
-                'solution': idea.get('solution'),
-                'project_id': record.project_id.id,
-                'department_id': record.id,
-            })
+
+        for canva in canvas:
+            vals = {}
+
+            if idea := canva.get('ideas'):
+                ai_capability_ids = self.env['ai.canvas.capability']
+
+                for capability in idea.get('ai_capability'):
+                    ai_capability_ids += ai_capability_ids.search([
+                        ('name', '=', capability
+                         )], limit=1)
+
+                if idea.get('implementation_type') and isinstance(idea.get('implementation_type'), list):
+                    implementation_type = idea.get('implementation_type')[0]
+                else:
+                    implementation_type = idea.get('implementation_type')
+
+                vals.update({
+                    'name': idea.get('idea'),
+                    'needs': idea.get('needs'),
+                    'solution': idea.get('solution'),
+                    'project_id': record.project_id.id,
+                    'department_id': record.id,
+                    'ai_capability': [(4, ai_capability_id.id, False) for ai_capability_id in ai_capability_ids],
+                    'implementation_type': implementation_type,
+                })
+
+            if evaluation := canva.get('evaluation'):
+                vals.update({
+                    'value': evaluation.get('value'),
+                    'value_point': str(evaluation.get('value_point')),
+                    'feasibility_point': str(evaluation.get('feasibility_point')),
+                    'feasibility': evaluation.get('feasibility'),
+
+                })
+            self.env['project.task'].create(vals)
